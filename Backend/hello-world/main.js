1cccf1cf3836d8cd526647d0fb0dd484f231ade7
WojtekMs <wojtek.atmo@gmail.com>
2022-03-31 19:02:04 +0200

    Backend added endpoints
    Backend now connects to database and allows to retrieve and insert data into database.

--- Backend/hello-world/main.js
+++ Backend/hello-world/main.js
@@ -1,8 +1,52 @@
-const stworz_db = require ("./stworz_db")
-const stworz_kolekcje = require ("./stworz_kolekcje")
-const wprowadz_jeden_dokument = require ("./wprowadz_jeden_dokument")
+const express = require('express');
+const res = require('express/lib/response');
+const mongoose = require('mongoose');
+const Desk = require('./models/desk');
+const get_url = require('./utils');
 
-stworz_db.stworzdb("bazaprojekt")
-stworz_kolekcje.stworzkolekcje("bazaprojekt","uzytkownicy")
-var myobj = { name: "John Doe", lastTimeOnline: 124018032022, lastTimeInOffice: 130018032022, lastUsedDesk: 42, department: "IT", position: "Web Developer", supervisor: "Donald Trump", telephone: 123456789, email: "john.doe@cool-company.com" };
-wprowadz_jeden_dokument.wprowadzjedendokument("bazaprojekt","uzytkownicy",myobj)
+username = "zespol";
+password = "projektzespolowy";
+db_name = "bazaprojekt";
+server_port = 50000;
+
+const app = express();
+
+// connect to database
+var url = get_url(username, password, db_name);
+mongoose.connect(url)
+    .then((result) => app.listen(server_port))
+    .catch((err) => console.log(err));
+
+app.get('/', (request, response) => {
+    response.send('Hello!');
+});
+
+app.get('/desks', (request, response) => {
+    Desk.find()
+        .then((result) => {
+            response.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get('/add-desk', (request, response) => {
+    const desk1 = new Desk({
+        floor: Math.floor(Math.random() * 5) + 1,
+        number: Math.floor(Math.random() * 100) + 1
+    });
+
+    desk1.save()
+        .then((result) => {
+            response.send(result);
+
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.use((request, response) => {
+    response.status(404).send("This page does not exist!");
+});
