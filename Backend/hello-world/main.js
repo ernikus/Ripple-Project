222feedf44fff8959e8e14a45a4bb2eb283b5e6c
WojtekMs <wojtek.atmo@gmail.com>
2022-03-31 19:06:00 +0200

    Remove redundant include

--- Backend/hello-world/main.js
+++ Backend/hello-world/main.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const res = require('express/lib/response');
 const mongoose = require('mongoose');
 const Desk = require('./models/desk');
 const get_url = require('./utils');
