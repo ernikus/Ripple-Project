0166cc09a63574e033143df8582f08fc95a7977e
Prociu <102244030+Prociu@users.noreply.github.com>
2022-04-12 10:36:42 +0200

    Add files via upload

---
+++ login-page/login-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  public loginForm!: FormGroup
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
