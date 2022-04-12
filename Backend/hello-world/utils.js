0fcf0df53c08f58f6cdf6ed7acfc6381df05fc20
WojtekMs <wojtek.atmo@gmail.com>
2022-04-12 10:07:24 +0200

    Hotseat backend added as docker service

--- Backend/hello-world/utils.js
+++ Backend/hello-world/utils.js
@@ -1,8 +1,13 @@
+// keep in mind:
+// - below URL paramateres are necessary to connect
+//   - authMechanism=DEFAULT
+//   - authSource=admin (try to authenticate in the default mongo database: admin)
+// - the name of the mongo server in URL must be the name of the database service defined in docker-compose.yml
 function get_url(username, password, db_name) {
     username = encodeURIComponent(username);
     password = encodeURIComponent(password);
     db_name = encodeURIComponent(db_name);
-    return `mongodb://${username}:${password}@localhost:27017/${db_name}?authMechanism=DEFAULT&authSource=admin`;
+    return `mongodb://${username}:${password}@mongodb_container:27017/${db_name}?authMechanism=DEFAULT&authSource=admin`;
 }
 
 module.exports = get_url;
