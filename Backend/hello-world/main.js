e46aae227e6f039f734ef811b01d40ccb4e07952
WojtekMs <wojtek.atmo@gmail.com>
2022-04-25 00:31:40 +0200

    Basic get employees endpoint added

--- Backend/hello-world/main.js
+++ Backend/hello-world/main.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const Desk = require('./models/desk');
+const Employee = require('./models/employee');
 const get_url = require('./utils');
 
 username = "zespol";
@@ -9,11 +11,18 @@ db_name = "bazaprojekt";
 server_port = 50000;
 
 const app = express();
+app.use(express.json());
+
+// MOCK DATA
+const employees = JSON.parse(fs.readFileSync(`${__dirname}/employees.json`, 'utf-8'))
 
 // connect to database
 var url = get_url(username, password, db_name);
 mongoose.connect(url)
-    .then((result) => app.listen(server_port))
+    .then((result) => {
+        Employee.create(employees);
+        app.listen(server_port);
+    })
     .catch((err) => console.log(err));
 
 app.get('/', (request, response) => {
@@ -22,6 +31,17 @@ app.get('/', (request, response) => {
 
 app.get('/desks', (request, response) => {
     Desk.find()
+        .then((result) => {
+            response.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get('/employees', (request, response) => {
+    Employee.find(request.query)
+        .setOptions({ sanitizeFilter: true })
         .then((result) => {
             response.send(result);
         })
