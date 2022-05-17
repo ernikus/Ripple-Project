c7c2fd35166a9c86379a74f83d6033fd20d59ce0
TimKapa <mittkapa@gmail.com>
2022-05-17 09:28:58 +0200

    You can now add desks into the database using the /add-desk endpoint

--- Backend/hello-world/main.js
+++ Backend/hello-world/main.js
@@ -125,12 +125,12 @@ app.post('/add-reservation', (request, response) => {
 });
 
 app.get('/add-desk', (request, response) => {
-    const desk1 = new Desk({
-        floor: Math.floor(Math.random() * 5) + 1,
-        number: Math.floor(Math.random() * 100) + 1
+    const newDesk = new Desk({
+        floor: request.body.floor,
+        number: request.body.number
     });
 
-    desk1.save()
+    newDesk.save()
         .then((result) => {
             response.send(result);
 
