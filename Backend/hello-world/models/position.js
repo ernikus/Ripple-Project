7f2d1b18a054034895c5b0855d795bb51d8b23fe
WojtekMs <wojtek.atmo@gmail.com>
2022-06-13 14:28:05 +0200

    Added position data model to conform to frontend

---
+++ Backend/hello-world/models/position.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const positionSchema = new Schema({
+    posID: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+// model name is important, mongoose will look for 'positions' collection based on the model name
+const Position = mongoose.model("Position", positionSchema);
+module.exports = Position;
