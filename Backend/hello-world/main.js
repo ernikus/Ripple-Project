b6da5bcb47cd8240529db57fdc1c462d5302b5c2
WojtekMs <wojtek.atmo@gmail.com>
2022-06-14 12:45:07 +0200

    Change patch to put endpoint for edit-employee

--- Backend/hello-world/main.js
+++ Backend/hello-world/main.js
@@ -97,7 +97,7 @@ app.post('/add-employee', (request, response) => {
 });
 
 //Editing an employee - add id of employee to end of url e.g. localhost:50000/edit-employee/abcdefg123456 and in request body add elements you want to change
-app.patch('/edit-employee/:id', (request, response) => {
+app.put('/edit-employee/:id', (request, response) => {
     const _editemployeeid = request.params.id;
     const updateEmployee = Employee.findByIdAndUpdate(_editemployeeid, request.body, {
         new: true
